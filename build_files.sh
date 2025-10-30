51b693847b1d3c2f37a6a331a6e3396af8d77b14
GusISilva <gustavoigorks@gmail.com>
2025-10-30 00:52:37 -0300

    Ajuste do build script para Vercel

--- build_files.sh
+++ build_files.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 set -e
 
-# Ativa o ambiente virtual se você estiver usando
-# source venv/bin/activate
-
 echo "Instalando dependências..."
-pip install -r requirements.txt
-
-echo "Aplicando migrações..."
-python manage.py migrate --noinput
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
 
 echo "Coletando arquivos estáticos..."
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 
 echo "Build concluído com sucesso!"
