e1260ad847de9f778d33f4c52271fe4347406c61
GusISilva <gustavoigorks@gmail.com>
2025-10-30 00:44:14 -0300

    alteracao no build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,16 @@
-python3 -m pip install -r requirements.txt
-python3 manage.py collectstatic --noinput
+#!/bin/bash
+set -e
+
+# Ativa o ambiente virtual se você estiver usando
+# source venv/bin/activate
+
+echo "Instalando dependências..."
+pip install -r requirements.txt
+
+echo "Aplicando migrações..."
+python manage.py migrate --noinput
+
+echo "Coletando arquivos estáticos..."
+python manage.py collectstatic --noinput
+
+echo "Build concluído com sucesso!"
